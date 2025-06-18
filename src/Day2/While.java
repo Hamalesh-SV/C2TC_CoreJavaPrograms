434fec26d14f6ae9a7b18ec035d571a1370acae6
ACT-C2-04 <ACT-C2-04@DESKTOP-I4SCAC5>
2025-06-18 12:24:38 +0530

    Today I worked on While Loop Java program

---
+++ src/Day2/While.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=91;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
