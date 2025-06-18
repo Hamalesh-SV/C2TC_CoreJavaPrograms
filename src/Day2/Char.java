4bf1ca20c51e7aec447e9caa93b84b5728c139ac
ACT-C2-04 <ACT-C2-04@DESKTOP-I4SCAC5>
2025-06-18 12:08:09 +0530

    Today I worked on Char Program in Java

---
+++ src/Day2/Char.java
@@ -0,0 +1,23 @@
+package Day2;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'c';
+		System.out.println(ch);
+
+		char ch1 = 80;
+		System.out.println(ch1);
+
+		char var1 = '\u00B6';
+		System.out.println(var1);
+
+		char var2 = '\u20AC';
+		System.out.println(var2);
+
+		int a = 'B';
+		System.out.println(a);
+	}
+
+}
