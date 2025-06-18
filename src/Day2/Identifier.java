c74688b905fd1271f25af03baab425cdf5e515ec
ACT-C2-04 <ACT-C2-04@DESKTOP-I4SCAC5>
2025-06-18 12:15:07 +0530

    Today I worked on Identifier Program in Java

---
+++ src/Day2/Identifier.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 23; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Geosha"; //use camelcase
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
