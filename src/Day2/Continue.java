a67b7d5bb3c4934e8ca1ebb4a109643cb399adf0
ACT-C2-04 <ACT-C2-04@DESKTOP-I4SCAC5>
2025-06-18 12:10:15 +0530

    Today I worked on Continue program in Java

---
+++ src/Day2/Continue.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 15; i++)
+		{
+		
+		if (i%2 != 0)
+		continue;
+		
+		System.out.print(i + " ");
+		}
+	}
+
+}
