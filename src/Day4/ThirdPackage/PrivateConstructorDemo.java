80288104a208608258464b1e2e4e7cf89d7444e0
ACT-C2-04 <ACT-C2-04@DESKTOP-I4SCAC5>
2025-06-20 09:50:43 +0530

    Today I worked on Private constructor program in Java

---
+++ src/Day4/ThirdPackage/PrivateConstructorDemo.java
@@ -0,0 +1,17 @@
+package Day4.ThirdPackage;
+
+public class PrivateConstructorDemo {
+	public static void main(String s[]) {
+		// Can't create object with private constructor
+		// MyClass m1=new MyClass();
+		
+		Myclass m=Myclass.getObject();
+		m.setId(10);
+		
+		Myclass m1=Myclass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
+}
